0b492ed85791f2bf77cd2c6737c469668898300b
Igor <yagr71@yandex.ru>
2022-04-20 22:39:09 +0400

    Add task_7

---
+++ task_7/Program.cs
@@ -0,0 +1,21 @@
+﻿int a;
+int n = new Random ().Next (10 , 10000);
+Console.WriteLine(n);
+
+
+if (n < 100)
+{
+    Console.WriteLine ("третьей цифры нет");
+}
+if (n > 100 && n < 1000)
+{
+    a = n % 10;
+    Console.WriteLine (a);
+}
+
+if (n > 1000 && n < 10000)
+{
+    a = n % 100;
+    a = a / 10;
+    Console.WriteLine (a);
+}
